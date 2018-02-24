30a8bc560c0571bdd0bc0439bd81f0769a3d754c
ffacon <francois.facon@gmail.com>
2018-02-24 10:11:23 +0100

    Update Home

--- ng2ecom/src/app/home/home.component.ts
+++ ng2ecom/src/app/home/home.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
+import {NewsService} from '../services/news.service';
+import {News} from '../beans/news';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+   providers: [NewsService]
+
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  message: string = 'Welcome in our shop!!!';
+	news: News[];
+	newsOfTheDay: News= {};
+	nextNews: News = {};
 
-  ngOnInit() {
-  }
+
+	constructor(private newsService: NewsService) {}
+
+	ngOnInit(){
+		this.updateNews();
+	}
+
+	updateNews= () => {
+		this.newsService
+			.getNews()
+			.then((news: News[]) => {
+				this.news = news;
+			});
+
+		this.newsService.randomNews()
+			.then((news: News) => {
+				this.newsOfTheDay = news;
+			});
+	}
+
+	addLike= (news: News) => {
+		this.newsService.addLike(news);
+	}
+
+	deleteNews= (news: News) => {
+		this.newsService.deleteNews(news)
+			.then(() => this.updateNews());
+	}
+
+	addNews= () => {
+		this.newsService.addNews(this.nextNews)
+		.then((addedNews: News) => this.news.push(addedNews) );
+	}
+
 
 }
