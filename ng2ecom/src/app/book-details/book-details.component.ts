0b50b2b66527759fcf54694c43c21bdeb0c1071f
ffacon <francois.facon@gmail.com>
2018-02-27 10:13:00 +0100

    Update bookDetails

--- ng2ecom/src/app/book-details/book-details.component.ts
+++ ng2ecom/src/app/book-details/book-details.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import {UserService} from '../services/user.service';
+import {BooksService} from '../services/books.service';
+import {Book} from '../beans/book';
+import { ActivatedRoute  }  from '@angular/router';
+
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  public book: Book;
+  private sub: any;
+  private bookId :number;
 
-  ngOnInit() {
-  }
+	constructor(
+		private route: ActivatedRoute,
+		public bookService: BooksService,
+		public userService: UserService){}
+
+	ngOnInit(): void {
+		this.sub = this.route.params.subscribe( params => {
+		this.bookId = +params['id']; 
+		// (+) converts string 'bookId' to a number
+        // In a real app: dispatch action to load the details here.
+    });
+		
+
+
+		this.bookService.getBook(this.bookId)
+			.then((book: Book) => {
+				this.book = book;
+			});
+	}
+
+
+	getImagePath = (): string => {
+		return '';
+	}
+
+
+	getStarsImagePath= (): string => {
+		return '';
+	}
+
 
 }
