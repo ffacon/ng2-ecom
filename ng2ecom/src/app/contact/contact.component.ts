cf1218618c66d7fe54cfe0bbf0058f0f5bcb0bca
ffacon <francois.facon@gmail.com>
2018-03-11 16:27:26 +0100

    PW03 add contact component

--- ng2ecom/src/app/contact/contact.component.ts
+++ ng2ecom/src/app/contact/contact.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  public email: string= "ecom@worldline.com";
+  phone: string= "+33 xxxxxxxx" 
+  address: string= "rue de la pointe Seclin"
+  messageSend: boolean=false;
+  message="";
 
   constructor() { }
+  
+  updateTextContent  = (data:any) =>{
+    this.message = data.target.value;
+    console.log(this.message);
+  }
+
+  sendMessage = () => {
+    console.log("message send: " + this.message);
+    this.messageSend =true;
+  }
 
   ngOnInit() {
+
   }
 
 }
