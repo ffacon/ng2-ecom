584ee52646e0eb347507f45a5b7930d3f4108843
ffacon <francois.facon@gmail.com>
2018-03-14 14:42:23 +0100

    PW06 Add ReactiveForm

--- ng2ecom/src/app/app.module.ts
+++ ng2ecom/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 
@@ -42,6 +42,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     HttpClientModule,
     routing
