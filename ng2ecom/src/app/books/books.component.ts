ba90ba2be0d1e0cb30c83039ef2f614c100304bd
ffacon <francois.facon@gmail.com>
2018-03-18 14:11:10 +0100

    PW07 Update books component

--- ng2ecom/src/app/books/books.component.ts
+++ ng2ecom/src/app/books/books.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../services/books.service';
 import { Book } from '../beans/book';
+import { KPagination } from '../components/kpagination/kpagination';
+import { DataContainerService } from '../services/data-container.service';
 
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
+  providers: [DataContainerService]
 })
 export class BooksComponent implements OnInit {
   books: Book[];
-  constructor(public booksService: BooksService ) { }
+  currentPage : number = 1;
+  booksPerPageFilter: number = 4;
+  bookNameFilter: string ="";
+  bookOrderBy: string = 'name';
+  reverseOrderFilter: boolean = false;
+
+
+  constructor(public booksService: BooksService,
+              public datacontainer:DataContainerService ) { }
 
   ngOnInit() { 
       this.booksService.getBooks()
@@ -19,4 +30,8 @@ export class BooksComponent implements OnInit {
       })
   }
 
+  switchPage(page:number)  {
+    this.currentPage = page;
+  }
+  
 }
