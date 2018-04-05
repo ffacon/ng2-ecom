c6cd085a18c5bb2ba8f4381c7f5da0b0d0975d05
ffacon <francois.facon@gmail.com>
2018-04-05 11:23:55 +0200

    PW08 Create buyZone component

---
+++ ng2ecom/src/app/buy-zone/buy-zone.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Book } from '../beans/book';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'buy-zone',
+  templateUrl: './buy-zone.component.html',
+  styleUrls: ['./buy-zone.component.css']
+})
+export class BuyZoneComponent implements OnInit {
+
+  @Input('book') book: Book;
+
+  constructor(public userService: UserService,public router: Router) { }
+
+  ngOnInit() {
+  }
+
+  addToBasket(book: Book) {
+    this.userService.basket.addProduct(book);
+    this.router.navigate(['basket']);
+  }
+
+}
