9a9c09336ddd61202c8b867abfeb03051ebb6f8a
ffacon <francois.facon@gmail.com>
2018-04-06 11:24:33 +0200

    PW08 Implement login

--- ng2ecom/src/app/login/login.component.ts
+++ ng2ecom/src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import {User} from '../beans/user';
+import {UserService} from '../services/user.service';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private userService: UserService) { }
+
+  errorMessage: string= undefined;
+  login: string;
+  password: string;
 
   ngOnInit() {
+  }
+
+  logUserWIthObservable() {
+    
+    this.userService.login(this.login,this.password).subscribe(
+        (user:User) => { this.router.navigate(['home'])}, // success path
+        error =>  this.errorMessage = `server status: ${error}`
+      );
+    }
+
+  logUser() {
+    this.userService.login(this.login,this.password).toPromise()
+    .then((user:User) => {
+      this.router.navigate(['home']);
+    }).catch((err ) => {
+      this.errorMessage = `server status: ${err}`;
+    });
   }
 
 }
