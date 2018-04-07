a8510ff2ce66c4d62f98dd5367d7bc1741afdfd5
ffacon <francois.facon@gmail.com>
2018-04-07 11:25:32 +0200

    PW08 Implement Basket

--- ng2ecom/src/app/basket/basket.component.ts
+++ ng2ecom/src/app/basket/basket.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../beans/book';
+import { Basket } from '../beans/basket';
+import { Item } from '../beans/item';
+import { BooksService } from '../services/books.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-basket',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasketComponent implements OnInit {
 
-  constructor() { }
+  items: Item[];
+
+  constructor(private userService: UserService) {  }
 
   ngOnInit() {
+    this.updateItems();
   }
+
+  updateItems() {
+    this.items = this.userService.basket.getItems();
+  }  
+
+  removeProduct(book: Book)  {
+    this.userService.basket.removeProduct(book);
+    this.updateItems();
+    return false;
+  }
+
+  getTotal = () : number => {
+    let res = 0;
+    this.items.forEach((item: Item) => { res += item.product.price * item.qty } );
+    return res;
+  } 
+
+  storeBasket = this.userService.storeBasket;
 
 }
