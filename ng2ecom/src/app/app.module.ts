85f724c1333cb33b7fcde600849a753a88ad75e2
ffacon <francois.facon@gmail.com>
2018-05-21 15:07:12 +0200

    PW6 Form & FormBuilder

--- ng2ecom/src/app/app.module.ts
+++ ng2ecom/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -37,6 +37,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule, 
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule, 
     routing
 
   ],
