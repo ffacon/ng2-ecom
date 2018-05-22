f90fb065cf30d08b316f1d8ec6975c2bb09e2e5d
ffacon <francois.facon@gmail.com>
2018-05-22 16:02:38 +0200

    PW7 P3 filtername

--- ng2ecom/src/app/services/data-container.service.ts
+++ ng2ecom/src/app/services/data-container.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Book } from '../../../../express/app/src/beans/book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataContainerService {
-
+  
+  filteredBooks: Book[];
   constructor() { }
 }
