dfba53c8e1c30203378b11c172e9b05e3e39d057
ffacon <francois.facon@gmail.com>
2018-05-22 15:51:37 +0200

    PW7 p2 reverse order

--- ng2ecom/src/app/books/books.component.ts
+++ ng2ecom/src/app/books/books.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../services/books.service';
 import { Book } from '../beans/book';
 import { KPagination } from '../components/kpagination/kpagination';
+import { FilterFieldPipe } from '../pipes/filter-field.pipe';
+import { UpdateDataPipe } from '../pipes/update-data.pipe';
+import { OrderByPipe } from '../pipes/order-by.pipe';
+
 
 @Component({
   selector: 'app-books',
@@ -12,6 +16,12 @@ export class BooksComponent implements OnInit {
   booksPerPageFilter: number = 4;
   currentPage: number = 1; 
   books: Book[];
+
+  bookNameFilter: string = '';
+  bookOrderBy: string = 'name';
+  reverseOrderFilter: boolean = false;
+
+
   constructor(public booksService: BooksService) { }
 
   ngOnInit() {
