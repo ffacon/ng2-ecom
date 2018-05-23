f385c46372389b95463005ccc4d64f070276e07f
ffacon <francois.facon@gmail.com>
2018-05-23 17:23:36 +0200

    PW3 Basket

--- ng2ecom/src/app/basket/basket.component.ts
+++ ng2ecom/src/app/basket/basket.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from '../beans/item';
+import { UserService } from '../services/user.service';
+import { Book } from '../beans/book';
 
 @Component({
   selector: 'app-basket',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasketComponent implements OnInit {
 
-  constructor() { }
+  items: Item[];
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+     this.updateItems();
+   }
+
+   updateItems() {
+    this.items = this.userService.basket.getItems();
+  }  
+
+  removeProduct(book: Book)  {
+    this.userService.basket.removeProduct(book);
+    this.updateItems();
+    return false;
   }
+
+  getTotal = () : number => {
+    let res = 0;
+    this.items.forEach((item: Item) => { res += item.product.price * item.qty } );
+    return res;
+  } 
+
+  storeBasket = this.userService.storeBasket;
 
 }
