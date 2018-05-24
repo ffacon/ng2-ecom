367fe154d2d86365bef2ba7a96204ebac3ab87e6
ffacon <francois.facon@gmail.com>
2018-05-24 14:27:16 +0200

    PW9 Pipe tests

--- ng2ecom/src/app/pipes/order-by.pipe.spec.ts
+++ ng2ecom/src/app/pipes/order-by.pipe.spec.ts
@@ -1,8 +1,46 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
 import { OrderByPipe } from './order-by.pipe';
+
+interface Person{
+		firstName: string,
+		lastName: string,
+		yearOfBirth: number
+	};
+
+let person0: Person,
+		person1: Person,
+		person2: Person;
+
+let persons: Person[];
+
+let pipe: OrderByPipe;
+
+beforeEach(() => {
+		person0 = { firstName: 'lou', lastName: 'reed', yearOfBirth: 1942 };
+		person1 = { firstName: 'michael', lastName: 'jackson', yearOfBirth: 1958 };
+		person2 = { firstName: 'grace', lastName: 'slick', yearOfBirth: 1939 };
+		
+		persons = [person0, person1, person2];
+
+		pipe = new OrderByPipe();
+	});
+
 
 describe('OrderByPipe', () => {
   it('create an instance', () => {
     const pipe = new OrderByPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('Filter on string, no reverse', function() {
+
+			pipe.transform(persons, 'firstName');
+
+			expect(persons[0]).toBe(person2);
+			expect(persons[1]).toBe(person0);
+			expect(persons[2]).toBe(person1);
+
+		});
 });
--- ng2ecom/src/app/pipes/update-data.pipe.spec.ts
+++ ng2ecom/src/app/pipes/update-data.pipe.spec.ts
@@ -1,2 +1,34 @@
+import {ReflectiveInjector, Injector} from '@angular/core';
+import { TestBed, async } from '@angular/core/testing';
 import { UpdateDataPipe } from './update-data.pipe';
-import { DataContainerService } from '../services/data-container.service';
+
+import {DataContainerService} from '../services/data-container.service';
+
+let parentInjector: Injector;
+let dataContainerMock: any;
+
+	beforeEach(() => {
+
+		dataContainerMock = { filteredBooks: '' };
+    TestBed.configureTestingModule({
+      
+       providers: [
+         {provide: DataContainerService, useClass: dataContainerMock}
+      ]
+    }).compileComponents();
+	});
+
+describe('UpdateDataPipe', () => {
+  it('create an instance', () => {
+    const pipe = new UpdateDataPipe(dataContainerMock);
+    expect(pipe).toBeTruthy();
+  });
+
+  it('Update data works', function() {
+			let pipe = new UpdateDataPipe(dataContainerMock);
+			let newContent: string = 'new field content';
+			pipe.transform(newContent, []);
+
+			expect(dataContainerMock.filteredBooks).toEqual(newContent);
+		});
+});
