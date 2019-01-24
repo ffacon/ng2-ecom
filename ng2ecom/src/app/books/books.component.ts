daae67167f00b57aded3a8992254aeedfa1a43a3
ffacon <francois.facon@gmail.com>
2019-01-24 16:36:07 +0100

    PW04 Update Books

--- ng2ecom/src/app/books/books.component.ts
+++ ng2ecom/src/app/books/books.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../beans/book';
+import { BooksService } from '../services/books.service';
 
 @Component({
   selector: 'app-books',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-
-  constructor() { }
+  books: Book[];
+  constructor(public booksService: BooksService) { }
 
   ngOnInit() {
+    this.booksService.getBooks().subscribe(
+      (books: Book[]) => { this.books = books; });
   }
 
 }
