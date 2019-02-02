80f032f35a60775f7fda000b80fbb43957a340dc
ffacon <francois.facon@gmail.com>
2019-02-02 15:40:48 +0100

    PW05 use contact service

--- ng2ecom/src/app/contact/contact.component.ts
+++ ng2ecom/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../services/contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,20 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  public email = 'ecom@worldline.com';
-  phone = '+33 xxxxxxxx';
-  address = 'rue de la pointe Seclin';
+  public email: string ;
+  phone: string;
+  address: string;
   messageSend = false;
-  message = '';
+  message: string;
 
-  constructor() { }
+  constructor(contactService: ContactService) {
+    this.email = contactService.email ;
+    this.phone = contactService.phone ;
+    this. address = contactService.address;
+}
 
   updateTextContent(data: any) {
     this.message = data.target.value;
     console.log(this.message);
   }
 
-  sendMessage() {
+  sendMessage = () => {
     console.log('message send: ' + this.message);
     this.messageSend = true;
   }
--- ng2ecom/src/app/services/contact.service.ts
+++ ng2ecom/src/app/services/contact.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class ContactService {
+
+  email = 'ecommerce@worldline.com';
+  phone = '+33 XXXXXXXXX';
+  address = 'Rue de la pointe, 59113 Seclin';
 
   constructor() { }
 }
