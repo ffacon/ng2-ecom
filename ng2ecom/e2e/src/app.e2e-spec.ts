f4f7deb6ac5d5bbee13fde9157355afdbaf5e63f
ffacon <francois.facon@gmail.com>
2019-08-26 11:16:17 +0200

    PW10 e2e test

--- ng2ecom/e2e/src/app.e2e-spec.ts
+++ ng2ecom/e2e/src/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import { AppPage } from './app.po';
+import { Ng2EComBooksPage } from './app.books';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('ng2-e-com App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -20,4 +21,50 @@ describe('workspace-project App', () => {
       level: logging.Level.SEVERE,
     } as logging.Entry));
   });
+});
+
+describe('ng2-e-com books', () => {
+  let page: Ng2EComBooksPage;
+  beforeEach(() => {
+     page = new Ng2EComBooksPage();
+  });
+
+  it('should display message saying ', () => {
+       page.navigateTo();
+       expect(page.getParagraphText()).toContain('ALL THE BOOKS YOU NEED, AT THE HIGHEST PRICE!');
+    });
+
+  beforeEach(() => {
+      page = new Ng2EComBooksPage();
+    });
+
+  it('should have 4 books in the page by default', () => {
+
+    const defaultNbBooks = 4;
+    const booksList = page.getBooksList();
+    expect(booksList.count()).toEqual(defaultNbBooks);
+   });
+
+  it('Update books per page value', () => {
+
+      const updateNbBooks = 2;
+      const input = page.getInput4nbPerPage();
+
+      input.clear();
+      input.sendKeys('2');
+      const booksList = page.getBooksList();
+
+      expect(booksList.count()).toEqual(updateNbBooks);
+    });
+
+  it('Check the filter by name', () => {
+      const filterValue = 'NINJA';
+      const input = page.getInput4bookName();
+
+      input.clear();
+      input.sendKeys(filterValue);
+      const booksList = page.getBooksList();
+
+      expect(booksList.count()).toEqual(1);
+    });
 });
